6fe62c9c8ebd4d3d2c0a0ae5a17a591b4262b060
kengo <mathematics9973@gmail.com>
2023-03-03 16:42:18 +0900

    fix comments destroy error

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,16 +9,21 @@ class CommentsController < ApplicationController
   def destroy
     @comment = @blog.comments.find(params[:id])
     @comment.destroy
+    # binding.pry
     redirect_to blog_path(params[:blog_id])
     # redirect_to blog_path(@blog)でもOK
-
+    # redirect_to blog_pathとすると、明示的ではないが
+    # redirect_to blog_path(params[:id])とrailsに認識される
+    # params[:id]はblogに関連付けられたcommentのidなので
+    # コメント削除前のblog投稿先に正しく戻れなくなる。
+    # よって、redirect_to blog_path(params[:blog_id])
+    # と記述して明示的にblogのidを取得しに行かなければならない。
   end
 
   private
 
   def set_blog
     @blog = Blog.find(params[:blog_id])
-    # binding.pry
   end
 
   def comment_params
